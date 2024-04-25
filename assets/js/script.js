26d8ae9f61f379bb8dc449a86ffa11548245814f
DICAVI86 <delcastillovial@gmail.com>
2024-04-25 16:01:53 -0400

    Se arreglo el script para agregar contadores y se cmabiaron las funciones para crear los botones para eliminar y chechbox de las tareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,16 @@ document.getElementById('input').addEventListener('submit', function (e) {
     if (nuevaTarea === '') {
         alert('Por favor anota una tarea.');
         return;
-    }
+    } 
 
     const listaTareas = document.getElementById('listaTareas');
     const li = document.createElement('li');
-    li.textContent = `${nuevaTarea}`;
-    li.appendChild(createDeleteButton());
-    li.appendChild(createCheckbox());
+    li.innerHTML = `
+    <span>${nuevaTarea}</span>
+    <input type="checkbox">
+    <button class="delete">❌</button>
+    `;
+
     listaTareas.appendChild(li);
 
     updateStats();
@@ -19,35 +22,21 @@ document.getElementById('input').addEventListener('submit', function (e) {
     document.getElementById('nuevaTarea').value = '';
 });
 
-function createDeleteButton() {
-    const deleteButton = document.createElement('button');
-    deleteButton.classList.add('delete')
-    deleteButton.textContent = 'X';
-    deleteButton.onclick = function (e) {
+document.getElementById('listaTareas').addEventListener('click', function (e) {
+    if (e.target.classList.contains('delete')) {
         e.target.parentNode.remove();
         updateStats();
-    };
-    return deleteButton;
-}
-
-function createCheckbox() {
-    const checkbox = document.createElement('input');
-    checkbox.type= 'checkbox';
-    return checkbox;
-}
-
-document.getElementById('input').addEventListener('click', function (e) {
-    if (e.target.type === 'checkbox') {
-        e.target.parentNode.classList.toggle('completed');
+    } else if (e.target.type === 'checkbox') {
+        e.target.parentNode.classList.toggle('completada');
         updateStats();
     }
 });
 
 function updateStats() {
     const totalTareas = document.querySelectorAll('#listaTareas li').length;
-    //const tareasRealizadas = document.querySelectorAll('#tareasRealizadas').length;
+    const tareasRealizadas = document.querySelectorAll('#listaTareas input[type="checkbox"]:checked').length;
     document.getElementById('totalTareas').textContent = totalTareas;
-    //document.getElementById('tareasRealizadas').textContent = tareasRealizadas;
+    document.getElementById('tareasRealizadas').textContent = tareasRealizadas;
 }
 
 
